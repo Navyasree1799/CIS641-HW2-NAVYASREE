71ccb92ab27eceb09c282d1c6b8fa26c3e543c82
Navyasree1799 <putlurinavyasree@gmail.com>
2023-10-15 21:56:14 -0400

    jason changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/CIS641-HW2-NAVYASREE",
+  base: "/CIS641-HW2-NAVYASREE",
   plugins: [react()],
 })
